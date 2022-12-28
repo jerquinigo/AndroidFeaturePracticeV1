6a8ee98271dbc367fea8797bb078c1d2666a06d2
jerquinigo <jonathanerquinigo@pursuit.org>
2022-12-27 22:14:28 -0500

    added secondActivityWindow with navigation

--- app/src/main/java/com/example/favoriesapp/MainActivity.java
+++ app/src/main/java/com/example/favoriesapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.favoriesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView image1;
     ImageView image2;
+    Button secondActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +24,17 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("hello");
         image1 = findViewById(R.id.imageView5);
         image2 = findViewById(R.id.imageView4);
+        secondActivity = findViewById(R.id.secondActivityButton);
         Picasso.get().load("https://cdna.artstation.com/p/assets/images/images/023/218/190/large/z-w-gu-spooky.jpg?1578494624").into(image1);
         Picasso.get().load("https://www.psu.com/wp/wp-content/uploads/2021/05/far-cry-6-ps4-ps5-release-date-dani-rojas.jpg").into(image2);
     }
 
-//    public void getImage1() {
-//
-//        Picasso.get().load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3oO-z-aP4QQMOMHH0TGdpHyiO6BX0GB8gFSp17ckSlhZofISUfUuIRrqFzm8TTqacg-8&usqp=CAU").into(image1);
-////        return image1;
-//    }
 
 
-    //    public void test() {
-//        Picasso.get().load()
-//    }
-
-
-
+    public void goToSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
 
 
 
---
+++ app/src/main/java/com/example/favoriesapp/SecondActivity.java
@@ -0,0 +1,33 @@
+package com.example.favoriesapp;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+        System.out.println("hello");
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
